48ac1718a1f968c5c9d1ab5afe28bd090d46f4b2
jbmakib <mdfoujolmobin@gmail.com>
2021-10-29 08:04:10 +0600

    route added for login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home/Home";
+import Login from "./Pages/Login/Login/Login";
 import Header from "./Pages/Shared/Header/Header";
 
 function App() {
@@ -13,6 +14,12 @@ function App() {
                 </Route>
                 <Route path="/home">
                     <Home></Home>
+                </Route>
+                <Route path="/login">
+                    <Login></Login>
+                </Route>
+                <Route path="*">
+                    <h2>Page not found</h2>
                 </Route>
             </Switch>
         </Router>
