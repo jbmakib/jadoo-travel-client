10df11e1a4f75b77920332ad1075cbd9dca9ecf8
jbmakib <mdfoujolmobin@gmail.com>
2021-10-29 08:32:30 +0600

    authprovider used in app js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import AuthProvider from "./contexts/AuthProvider";
 import Home from "./Pages/Home/Home/Home";
 import Login from "./Pages/Login/Login/Login";
 import Header from "./Pages/Shared/Header/Header";
 
 function App() {
     return (
-        <Router>
-            <Header></Header>
-            <Switch>
-                <Route exact path="/">
-                    <Home></Home>
-                </Route>
-                <Route path="/home">
-                    <Home></Home>
-                </Route>
-                <Route path="/login">
-                    <Login></Login>
-                </Route>
-                <Route path="*">
-                    <h2>Page not found</h2>
-                </Route>
-            </Switch>
-        </Router>
+        <AuthProvider>
+            <Router>
+                <Header></Header>
+                <Switch>
+                    <Route exact path="/">
+                        <Home></Home>
+                    </Route>
+                    <Route path="/home">
+                        <Home></Home>
+                    </Route>
+                    <Route path="/login">
+                        <Login></Login>
+                    </Route>
+                    <Route path="*">
+                        <h2>Page not found</h2>
+                    </Route>
+                </Switch>
+            </Router>
+        </AuthProvider>
     );
 }
 
