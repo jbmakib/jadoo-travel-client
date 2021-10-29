cfe7846212c5a42ebde88152c46ed463deeba746
jbmakib <mdfoujolmobin@gmail.com>
2021-10-29 08:51:45 +0600

    login with google button style added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./Login.css";
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className="container text-center">
+            <h2>Please login Using</h2>
+            <button className="btn btn-lg btn-google btn-block text-uppercase btn-outline">
+                <img
+                    src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
+                    alt="Google logo"
+                    width="50px"
+                />
+            </button>
         </div>
     );
 };
