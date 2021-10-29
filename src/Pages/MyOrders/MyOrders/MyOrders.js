8990a63043961575c2c19df781070cb8737e51f3
jbmakib <mdfoujolmobin@gmail.com>
2021-10-29 22:07:27 +0600

    my orders added

---
+++ src/Pages/MyOrders/MyOrders/MyOrders.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyOrders = () => {
+    return (
+        <div className="container">
+            <h2 className="text-center">All of your orders</h2>
+        </div>
+    );
+};
+
+export default MyOrders;
