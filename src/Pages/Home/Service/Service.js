60e70ebc0b6a3bbe9c1a6826189ac6db6fe515ff
jbmakib <mdfoujolmobin@gmail.com>
2021-10-29 21:32:04 +0600

    service component updated

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Service.css";
 
-const Service = ({ service: { name, img, price, features, duration } }) => {
+const Service = ({
+    service: { name, img, price, features, duration, ratings },
+}) => {
     return (
         <div className="col">
             <div className="h-100 rounded-4 overflow-hidden card">
@@ -13,8 +15,10 @@ const Service = ({ service: { name, img, price, features, duration } }) => {
                 <div className="card-body">
                     <div className="fw-bold border-bottom pb-3 border-3 card-border">
                         <small>
-                            <i className="far fa-calendar text-orange"></i>
-                            <span className="text-muted ps-2">Mon-Fri</span>
+                            <i className="fas fa-star text-orange"></i>
+                            <span className="text-muted ps-1">
+                                {ratings?.rate} ({ratings?.raters})
+                            </span>
                         </small>
                         <small className="ps-3">
                             <i className="far fa-clock text-orange"></i>
@@ -26,6 +30,9 @@ const Service = ({ service: { name, img, price, features, duration } }) => {
                         <small className="m-0 text-secondary">{features}</small>
                         <p className="h5 m-0 text-orange">${price}</p>
                     </div>
+                    <button className="rounded-pill px-3 py-1 mt-1 btn-outline-orange">
+                        Book
+                    </button>
                 </div>
             </div>
         </div>
