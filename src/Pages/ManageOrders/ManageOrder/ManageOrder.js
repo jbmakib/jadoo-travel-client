6f03c7a61d993702543c3dc51835c9b0aea69579
jbmakib <mdfoujolmobin@gmail.com>
2021-10-30 21:35:31 +0600

    manage orders updated

--- src/Pages/ManageOrders/ManageOrder/ManageOrder.js
+++ src/Pages/ManageOrders/ManageOrder/ManageOrder.js
@@ -1,11 +1,32 @@
 import React from "react";
 
-const ManageOrder = () => {
+const ManageOrder = ({
+    order: {
+        _id,
+        name,
+        start,
+        issued,
+        status,
+        service: { name: serviceName },
+    },
+}) => {
+    const handleChange = (e) => {
+        console.log(_id, e.target.value);
+    };
     return (
         <tr>
-            <td>pnc</td>
-            <td> (YYYY-MM-DD)</td>
-            <td>pnc</td>
+            <td>{name}</td>
+            <td>{serviceName}</td>
+            <td>{issued}</td>
+            <td>{start}</td>
+            <td>{status}</td>
+            <td>
+                <select defaultValue={status} onChange={handleChange}>
+                    <option value="Decline">Decline</option>
+                    <option value="Pending">Pending</option>
+                    <option value="Approve">Approve</option>
+                </select>
+            </td>
         </tr>
     );
 };
--- src/Pages/ManageOrders/ManageOrders/ManageOrders.js
+++ src/Pages/ManageOrders/ManageOrders/ManageOrders.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ManageOrder from "../ManageOrder/ManageOrder";
 
 const ManageOrders = () => {
+    const [allOrders, setAllOrders] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://ancient-brook-62418.herokuapp.com/orders`)
+            .then((res) => res.json())
+            .then((data) => setAllOrders(data))
+            .catch((err) => console.log(err.message));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     return (
         <div className="container">
-            <h2 className="text-center">All of your orders:</h2>
+            <h2 className="text-center">
+                All of the orders: {allOrders.length}
+            </h2>
             <table className="table">
                 <thead>
                     <tr>
+                        <th scope="col">User Name</th>
                         <th scope="col">Tour Name</th>
-                        <th scope="col">Starts</th>
+                        <th scope="col">Date Issued</th>
+                        <th scope="col">Tour Starts</th>
                         <th scope="col">Status</th>
+                        <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {/* {myOrders.map((order) => (
-                        <MyOrder key={order._id} order={order} />
-                    ))} */}
+                    {allOrders.map((order) => (
+                        <ManageOrder key={order._id} order={order} />
+                    ))}
                 </tbody>
             </table>
         </div>
