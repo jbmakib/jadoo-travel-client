5f778c97cfdb7c4a79ea9e977ac5411741c7099a
jbmakib <mdfoujolmobin@gmail.com>
2021-10-31 12:25:05 +0600

    our story created and added to home

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import OurStory from "../OurStory/OurStory";
 import Review from "../Review/Review";
 import Services from "../Services/Services";
 
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner />
             <Services />
             <Review />
+            <OurStory />
         </div>
     );
 };
---
+++ src/Pages/Home/OurStory/OurStory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OurStory = () => {
+    return (
+        <div>
+            <h2>This is our story</h2>
+        </div>
+    );
+};
+
+export default OurStory;
