9401d1469e0b6de67946ca4c4c7912e5eea606f9
jbmakib <mdfoujolmobin@gmail.com>
2021-10-31 10:07:01 +0600

    addservice style updated

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React from "react";
 import { useForm } from "react-hook-form";
+import "./AddService.css";
 
 const AddService = () => {
     const { register, handleSubmit, reset } = useForm();
@@ -26,49 +27,93 @@ const AddService = () => {
         <div className="container">
             <h2 className="text-center">Add a service</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input
-                    {...register("name", { required: true })}
-                    placeholder="Service Name"
-                />
-                <input
-                    type="number"
-                    {...register("price", { required: true })}
-                    placeholder="Service Price"
-                />
-                <input
-                    {...register("features", { required: true })}
-                    placeholder="Add main features"
-                />
-                <input
-                    type="url"
-                    {...register("img", { required: true })}
-                    placeholder="Image url"
-                />
-                <input
-                    type="number"
-                    {...register("duration", { required: true })}
-                    placeholder="Tour duration"
-                />
-                <input
-                    type="number"
-                    step="0.01"
-                    {...register("ratings.rate", {
-                        required: true,
-                        min: 0,
-                        max: 5,
-                    })}
-                    placeholder="Ratings"
-                />
-                <input
-                    type="number"
-                    {...register("ratings.raters", { required: true })}
-                    placeholder="Total Raters"
-                />
-                <textarea
-                    {...register("description", { required: true })}
-                    placeholder="Short description"
-                />
-                <input type="submit" />
+                <div className="form-floating mb-3">
+                    <input
+                        className="form-control"
+                        id="floatingServiceName"
+                        placeholder="Service Name"
+                        {...register("name", { required: true })}
+                    />
+                    <label htmlFor="floatingServiceName">Service Name</label>
+                </div>
+                <div className="form-floating mb-3">
+                    <input
+                        type="number"
+                        className="form-control"
+                        id="floatingServicePrice"
+                        placeholder="Service Price"
+                        {...register("price", { required: true })}
+                    />
+                    <label htmlFor="floatingServicePrice">Service Price</label>
+                </div>
+                <div className="form-floating mb-3">
+                    <input
+                        className="form-control"
+                        id="floatingMainFeatures"
+                        placeholder="Add main features"
+                        {...register("features", { required: true })}
+                    />
+                    <label htmlFor="floatingMainFeatures">
+                        Add main features
+                    </label>
+                </div>
+                <div className="form-floating mb-3">
+                    <input
+                        type="url"
+                        className="form-control"
+                        id="floatingImageUrl"
+                        placeholder="Image url"
+                        {...register("img", { required: true })}
+                    />
+                    <label htmlFor="floatingImageUrl">Image url</label>
+                </div>
+                <div className="form-floating mb-3">
+                    <input
+                        type="number"
+                        className="form-control"
+                        id="floatingTourDuration"
+                        placeholder="Tour Duration"
+                        {...register("duration", { required: true })}
+                    />
+                    <label htmlFor="floatingTourDuration">Tour Duration</label>
+                </div>
+                <div className="form-floating mb-3">
+                    <input
+                        type="number"
+                        step="0.01"
+                        className="form-control"
+                        id="floatingRatings"
+                        placeholder="Ratings"
+                        {...register("ratings.rate", {
+                            required: true,
+                            min: 0,
+                            max: 5,
+                        })}
+                    />
+                    <label htmlFor="floatingRatings">Ratings</label>
+                </div>
+                <div className="form-floating mb-3">
+                    <input
+                        type="number"
+                        className="form-control"
+                        id="floatingTotalRaters"
+                        placeholder="Total Raters"
+                        {...register("ratings.raters", { required: true })}
+                    />
+                    <label htmlFor="floatingTotalRaters">Total Raters</label>
+                </div>
+                <div className="form-floating mb-3">
+                    <textarea
+                        className="form-control add-service-textarea"
+                        id="floatingShortDescription"
+                        placeholder="Short description..."
+                        {...register("description", { required: true })}
+                    />
+                    <label htmlFor="floatingShortDescription">
+                        Short description...
+                    </label>
+                </div>
+                <input type="submit" className="btn btn-outline-orange" />
             </form>
         </div>
     );
